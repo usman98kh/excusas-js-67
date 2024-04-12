f347b1cfa49fb7aa5dbb5e9e03c6294d2b7c462d
usman98kh <117779193+usman98kh@users.noreply.github.com>
2024-04-12 19:00:06 +0000

    terminado

--- src/app.js
+++ src/app.js
@@ -5,7 +5,30 @@ import "./style.css";
 import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
-window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
+function excuseGenerator(who, action, what, when) {
+  let whoindex = Math.floor(Math.random() * who.length);
+  let actionindex = Math.floor(Math.random() * action.length);
+  let whatindex = Math.floor(Math.random() * action.length);
+  let whenindex = Math.floor(Math.random() * when.length);
+  console.log(who);
+  console.log(Math.random());
+
+  let excuse = `${who[whoindex]} ${action[actionindex]} ${what[whatindex]} ${when[whenindex]}`;
+  return excuse;
+}
+
+window.onload = () => {
+  let who = ["The dog", "My grandma", "The mailman", "My bird"];
+  let action = ["ate", "peed", "crushed", "broke"];
+  let what = ["my homework", "my phone", "the car"];
+  let when = [
+    "before the class",
+    "when I was sleeping",
+    "while I was exercising",
+    "during my lunch",
+    "while I was praying"
+  ];
+  const excuse = excuseGenerator(who, action, what, when);
+  document.getElementById("excuse").innerHTML = excuse;
+  console.log(excuse);
 };
